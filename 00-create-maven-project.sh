a707058d0f389f3b53b4e850981a8be8190adc77
Stéphane Philippart <wildagsx@gmail.com>
2025-02-21 10:23:44 +0100

    feat: 🦜 add LangChain4j with Maven (#2)

--- 00-create-maven-project.sh
+++ 00-create-maven-project.sh
@@ -3,4 +3,4 @@
 bat -P -r 5: $(basename "$0")
 
 # Create the Java project using Maven
-mvn archetype:generate -DgroupId=fr.wilda -DartifactId=maven-demo -Dversion=1.0.0-SNAPSHOT -DarchetypeArtifactId=maven-archetype-quickstart -DarchetypeVersion=1.5
+mvn archetype:generate -DgroupId=fr.wilda -DartifactId=maven-demo-create -Dversion=1.0.0-SNAPSHOT -DarchetypeArtifactId=maven-archetype-quickstart -DarchetypeVersion=1.5
