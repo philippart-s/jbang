16aa9fffa7d1bd46a34ed5446dbd7c3e603de8ac
philippart-s <wildagsx@gmail.com>
2025-03-24 10:50:55 +0000

    clear: 🗑️ remove code

--- jbang-demo/Jarvis.java
+++ jbang-demo/Jarvis.java
@@ -1,55 +1,24 @@
 ///usr/bin/env jbang "$0" "$@" ; exit $? 
 // 06-add-jarvis-deps
-//DEPS dev.langchain4j:langchain4j:1.0.0-beta1 dev.langchain4j:langchain4j-mistral-ai:1.0.0-beta1 ch.qos.logback:logback-classic:1.5.6
+
 // 07-add-external-resources
-//FILES resources/logback.xml
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import dev.langchain4j.memory.ChatMemory;
-import dev.langchain4j.memory.chat.MessageWindowChatMemory;
-import dev.langchain4j.model.mistralai.MistralAiStreamingChatModel;
-import dev.langchain4j.service.AiServices;
-import dev.langchain4j.service.SystemMessage;
-import dev.langchain4j.service.TokenStream;
-
 public class Jarvis {
 
-        private static final Logger _LOG = LoggerFactory.getLogger(Jarvis.class);
+    private static final Logger _LOG = LoggerFactory.getLogger(Jarvis.class);
 
-        // 08-ai-services-mode
-        interface Assistant {
-                @SystemMessage("Tu es JARVIS, un assistant virtuel expert dans le développement Java.")
-                TokenStream chat(String message);
-        }
+    // 08-ai-services-mode
 
-        public static void main(String[] args) {
-                // 09-mistral-model
-                MistralAiStreamingChatModel streamingChatModel = MistralAiStreamingChatModel.builder()
-                                .apiKey(System.getenv("OVH_AI_ENDPOINTS_ACCESS_TOKEN"))
-                                .modelName(System.getenv("OVH_AI_ENDPOINTS_MODEL_NAME"))
-                                .baseUrl(
-                                        System.getenv("OVH_AI_ENDPOINTS_MODEL_URL"))
-                                .maxTokens(512)
-                                .build();
+    public static void main(String[] args) {
+        // 09-mistral-model
 
-                // 10-add-memory
-                ChatMemory chatMemory = MessageWindowChatMemory.withMaxMessages(10);
+        // 10-add-memory
 
-                // 11-create-assistant
-                Assistant assistant = AiServices.builder(Assistant.class)
-                                .streamingChatLanguageModel(streamingChatModel)
-                                .chatMemory(chatMemory)
-                                .build();
+        // 11-create-assistant
 
-                // 12-prompt
-                _LOG.info("💬: Bonjour JARVIS. Explique en quelques lignes ce qu'est JBang à des développeuses et développeurs Java. Merci.\n");
-                TokenStream tokenStream = assistant
-                                .chat("Bonjour JARVIS. Explique en quelques lignes ce qu'est JBang à des développeuses et développeurs Java. Merci.");
-                _LOG.info("🤖: ");
-                tokenStream
-                                .onPartialResponse(_LOG::info)
-                                .onError(Throwable::printStackTrace).start();
-        }
+        // 12-prompt
+    }
 }
