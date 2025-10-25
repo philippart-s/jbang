0279808b619c51ec1037e23ee7aaf61d3a262e4f
Stéphane Philippart <stephane.philippart@ovhcloud.com>
2025-10-25 18:53:30 +0200

    feat: #️⃣ Use SDKMan for JBang installation

--- 02-jbang-installation.sh
+++ 02-jbang-installation.sh
@@ -2,9 +2,22 @@
 
 clear
 
-bat -P -r 9:10 $(basename "$0")
-
+# JBang installation
+echo "How install JBang?"
+echo "     1️⃣. curl -Ls https://sh.jbang.dev | bash -s - app setup"
+echo "     2️⃣. brew install jbangdev/tap/jbang"
+echo "     3️⃣. sdk install jbang"
+echo ""
 read -n 1 -p "Press any key to continue"
 
-# JBang installation
-curl -Ls https://sh.jbang.dev | bash -s - app setup
+clear
+
+bat -P -r 22: $(basename "$0")
+
+# Load SDKMAN into this script environment
+if [ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]; then
+    source "$HOME/.sdkman/bin/sdkman-init.sh"
+fi
+
+# Display current installed SDKs
+sdk current
