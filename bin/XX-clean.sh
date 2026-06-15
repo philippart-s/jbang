849f15378aead84e9128dcec4c00a91f6b3c32ea
Stéphane Philippart <stephane.philippart@ovhcloud.com>
2026-06-15 14:49:22 +0200

    feat: 🗑️ remove created templates

--- bin/XX-clean.sh
+++ bin/XX-clean.sh
@@ -14,6 +14,8 @@ jbang app uninstall jarvis-catalog
 jbang app uninstall httpd
 jbang alias remove jarvis-gh
 jbang catalog remove wildagsx
+jbang template remove jarvis-template
+rm ../template/logback.xml ../template/myapp.java ../template/Ultron.java
 
 read -n 1 -p "Press any key to continue\n"
 
